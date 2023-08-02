9e058442f0cecbd9ed1e7f75898a5f37a8ab9c2d
Chaplin <67743720+leechanggi@users.noreply.github.com>
2023-08-02 23:54:37 +0900

    스타일 작업

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
-
 function Search() {
-	return <div>Search</div>;
+	return (
+		<label htmlFor="search" className="search">
+			<input type="search" name="search" id="search" className="search_input" />
+			<button type="button" className="search_button">
+				<span className="sr-only">검색</span>
+			</button>
+		</label>
+	);
 }
 
 export default Search;

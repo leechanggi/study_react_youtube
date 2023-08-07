b9864366f3074800639df5e0a887e1b41f3cb307
Chaplin <67743720+leechanggi@users.noreply.github.com>
2023-08-07 22:24:56 +0900

    스타일 작업

--- src/components/VideoItem.tsx
+++ src/components/VideoItem.tsx
@@ -1,22 +1,14 @@
 import { Link } from "react-router-dom";
 
 export default function VideoItem({ data }: any) {
+  const snippet = data.snippet;
   return (
     <li className="video_item">
       <Link to={`/videos/watch/${data.id}`}>
-        <img
-          className="item_img"
-          src={data.snippet.thumbnails.medium.url}
-          alt={data.snippet.title}
-        />
+        <img className="item_img" src={snippet.thumbnails.medium.url} alt={snippet.title} />
         <div className="item_info">
-          <div className="info_img">
-            <img src={data.snippet.thumbnails.default.url} alt={data.snippet.channelTitle} />
-          </div>
-          <div className="info_data">
-            <p className="data_title ellipsis-2">{data.snippet.title}</p>
-            <p className="data_channelTitle ellipsis">{data.snippet.channelTitle}</p>
-          </div>
+          <p className="info_title ellipsis-2">{snippet.title}</p>
+          <p className="info_channelTitle ellipsis">{snippet.channelTitle}</p>
         </div>
       </Link>
     </li>

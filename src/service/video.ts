7125964359e14d86495e6b2b60d6d3673bc6fd1f
Chaplin <67743720+leechanggi@users.noreply.github.com>
2023-08-11 11:53:43 +0900

    작업

--- src/service/video.ts
+++ src/service/video.ts
@@ -2,52 +2,54 @@ import HttpClient from "../network/http";
 
 export default class VideoService {
   private http: HttpClient;
-  private API_KEY: string;
+  private API_KEY?: string;
 
-  constructor(httpClient: HttpClient, API_KEY: string) {
+  constructor(httpClient: HttpClient, API_KEY?: string) {
     this.http = httpClient;
     this.API_KEY = API_KEY;
   }
 
   async getVideoItems() {
-    return this.http
-      .fetch("/videos", {
-        method: "GET",
-        params: {
-          part: "snippet",
-          chart: "mostPopular",
-          maxResults: "25",
-          key: this.API_KEY,
-        },
-      })
-      .then((data) => data.items);
     // return this.http
-    //   .fetch("/data/hotTrend.json", {
+    //   .fetch("/videos", {
     //     method: "GET",
+    //     params: {
+    //       part: "snippet",
+    //       chart: "mostPopular",
+    //       maxResults: "25",
+    //       key: this.API_KEY,
+    //     },
     //   })
     //   .then((data) => data.items);
+
+    return this.http
+      .fetch("/data/hotTrend.json", {
+        method: "GET",
+      })
+      .then((data) => data.items);
   }
 
   async getVideoItemsFromKeyword(keyword: string) {
-    return this.http
-      .fetch("/search", {
-        method: "GET",
-        params: {
-          part: "snippet",
-          maxResults: "25",
-          q: keyword,
-          key: this.API_KEY,
-        },
-      })
-      .then((data) =>
-        data.items.map((item: { id: { videoId: any } }) => ({ ...item, id: item.id.videoId }))
-      );
     // return this.http
-    //   .fetch("/data/searchKeyword.json", {
+    //   .fetch("/search", {
     //     method: "GET",
+    //     params: {
+    //       part: "snippet",
+    //       maxResults: "25",
+    //       q: keyword,
+    //       key: this.API_KEY,
+    //     },
     //   })
     //   .then((data) =>
-    //     data.items.map((item: { id: { videoId: string } }) => ({ ...item, id: item.id.videoId }))
+    //     data.items.map((item: { id: { videoId: any } }) => ({ ...item, id: item.id.videoId }))
     //   );
+
+    return this.http
+      .fetch("/data/searchKeyword.json", {
+        method: "GET",
+      })
+      .then((data) =>
+        data.items.map((item: { id: { videoId: string } }) => ({ ...item, id: item.id.videoId }))
+      );
   }
 }
